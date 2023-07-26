853d665c84991d3565c4046bcdbaa70ba372c8ad
sharpsailor <sharpsailor13@gmail.com>
2023-07-26 11:23:26 +0530

    Rod Cutting

--- RodCutting.java
+++ RodCutting.java
@@ -1,5 +1,58 @@
 package AdityaVerma;
-
+import java.io.*;
+import java.util.*;
 public class RodCutting {
+	//{ Driver Code Start
 
+	    public static void main(String args[])throws IOException {
+	        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter out=new PrintWriter(System.out);
+	        
+	        int t = Integer.parseInt(in.readLine().trim());
+	        while (t-- > 0) {
+	            int n = Integer.parseInt(in.readLine().trim());
+	            String s[]=in.readLine().trim().split(" ");
+	            int[] arr = new int[n];
+	            for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(s[i]);
+
+	            Solution ob = new Solution();
+	            out.println(ob.cutRod(arr, n));
+	        }
+	        out.close();
+	    }
+	    
+	// } Driver Code Ends
 }
+
+	class Solution{
+	    public int cutRod(int price[], int n) {
+	        //code here
+	        int[] cuts = new int[n];
+	        for(int i =0;i<n;i++){
+	            cuts[i]= i+1;
+	        }
+	        return minCost(n, cuts , price);
+	    }
+	    public int minCost(int n, int[] cuts,int[] prices) {
+	        // Arrays.sort(cuts);
+	        int[][] dp = new int[n+1][cuts.length+1];
+	        for(int i =0;i<dp.length;i++){
+	            for(int j =0;j<dp[0].length;j++){
+	                if(i==0||j==0){
+	                    dp[i][j]=0;
+	                }
+	            }
+	        }
+	        for(int i =1;i<dp.length;i++){
+	            for(int j =1;j<dp[0].length;j++){
+	                if(cuts[i-1]<=j){
+	                    dp[i][j]= Math.max(dp[i-1][j],prices[i-1]+dp[i][j-cuts[i-1]]);
+	                }else{
+	                    dp[i][j]=dp[i-1][j];
+	                }
+	            }
+	        }
+	        return dp[n][cuts.length];
+	    }
+	}
+
--- TargetSum.java
+++ TargetSum.java
@@ -1,5 +1,33 @@
 package AdityaVerma;
 
 public class TargetSum {
+	class Solution {
+	    public int findTargetSumWays(int[] nums, int target) {
+	        int n = nums.length;
+	        int sum = 0;
+	        for(int x: nums){
+	            sum+=x;
+	        }
+	        if((sum- target)%2!=0 || target>sum){
+	            return 0;
+	        }
+	        sum = (sum- target)/2;
+	        return subSetSum(nums, n , sum);
+	    }
+	    public int subSetSum(int[] arr, int n , int sum ){
+	        int[][] t = new int[n+1][sum+1];
+	        t[0][0]=1 ;
+	        for(int i =1;i<=n;i++){
+	            for(int j =0;j<=sum;j++){
+	                if(arr[i-1]<=j){
+	                    t[i][j]= t[i-1][j]+t[i-1][j-arr[i-1]];
+	                }else{
+	                    t[i][j]= t[i-1][j];
+	                }
+	            }
+	        }
+	        return t[n][sum];
 
+	    }
+	}
 }
